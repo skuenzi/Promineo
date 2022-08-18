fca92fd486340a17854b66f507b207365beced9c
sara.kuenzi <48669509+skuenzi@users.noreply.github.com>
2022-08-17 23:05:30 -0600

    making console output clearer

--- Week3/week3.js
+++ Week3/week3.js
@@ -1,14 +1,14 @@
 // 1. Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
 const ages = [3, 9, 23, 64, 2, 8, 28, 93]
-console.log( ages)
+console.log( 'Ages:', ages)
 
 // a. Programmatically subtract the value of the first element in the array from the value in the last element of the array
 let ageRange = () => ages[ages.length-1] - ages[0]
-console.log(ageRange())
+console.log('Ages range:',ageRange())
 
 // b. Add a new age to your array and repeat the step above to ensure it is dynamic
 ages.push(45)
-console.log(ageRange())
+console.log('New ages range:',ageRange())
 
 // c. Use a loop to iterate through the array and calculate the average age.
 const getAverage = () => {
@@ -19,7 +19,7 @@ const getAverage = () => {
     return ageTotal/ages.length
 }
 
-console.log(getAverage())
+console.log('Ages average:',getAverage())
 
 // 2. Create an array called names that contains the following values: ‘Sam’, ‘Tommy’, ‘Tim’, ‘Sally’, ‘Buck’, ‘Bob’.
 
@@ -35,7 +35,7 @@ const averageNumLetters = () => {
     return totalLetters/names.length
 }
 
-console.log(averageNumLetters())
+console.log('Names average letters:',averageNumLetters())
 
 // b. Use a loop to iterate through the array again and concatenate all the names together, separated by spaces
 
@@ -47,15 +47,15 @@ const namesList = () => {
     return list
 }
 
-console.log(namesList())
+console.log('Names concat string:',namesList())
 
 // 3. How do you access the last element of any array? 
 
-console.log(names[names.length-1])
+console.log('Last element in array:', names[names.length-1])
 
 // 4. How do you access the first element of any array?
 
-console.log(names[0])
+console.log('First Element in array:',names[0])
 
 // 5. Create a new array called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.
 
@@ -65,7 +65,7 @@ for (let i = 0; i < names.length; i++) {
     namesLengths.push(names[i].length)
 }
 
-console.log(namesLengths)
+console.log('Names lengths array:',namesLengths)
 
 // 6. Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array. 
 
@@ -75,7 +75,7 @@ for (let i = 0; i < namesLengths.length; i++) {
     totalNamesLength += namesLengths[i]
 }
 
-console.log(totalNamesLength)
+console.log('Total characters in [names]:',totalNamesLength)
 
 // 7. Write a function that takes two parameters, word and n, as arguments and returns the word concatenated to itself n number of times
 
@@ -87,7 +87,7 @@ const words = (words, n) => {
     return result
 }
 
-console.log(words('Hello', 3))
+console.log('Repeated words:',words('Hello', 3))
 
 // 8.  Write a function that takes two parameters, firstName and lastName, and returns a full name
 
@@ -95,7 +95,7 @@ function fullName (firstName,lastName) {
     return firstName + ' ' + lastName
 }
 
-console.log(fullName('Sara', 'Kuenzi'))
+console.log('Full name:',fullName('Sara', 'Kuenzi'))
 
 // 9. Write a function that takes an array of numbers and returns true if the sum of all the numbers in the array is greater than 100.
 
@@ -111,8 +111,8 @@ function greaterThan100 (arr) {
     }
 }
 
-console.log(greaterThan100([20, 20, 20, 20]))
-console.log(greaterThan100([20, 20, 20, 20, 50]))
+console.log('Is the total of array [20, 20, 20, 20] greater than 100?', greaterThan100([20, 20, 20, 20]))
+console.log('Is the total of array [20, 20, 20, 50] greater than 100?',greaterThan100([20, 20, 20, 20, 50]))
 
 // 10. Write a function that takes an array of numbers and returns the average of all the elements in the array.
 
@@ -124,7 +124,7 @@ function arrAverage (arr) {
     return total/arr.length
 }
 
-console.log(arrAverage([20, 20, 20, 20]))
+console.log('Average of array [20, 20, 20, 20]:',arrAverage([20, 20, 20, 20]))
 
 // 11. Write a function that takes two arrays of numbers and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
 
@@ -144,7 +144,7 @@ function isGreater(arr1, arr2) {
     return arr1Average > arr2Average ? true : false
 }
 
-console.log(isGreater([20, 20, 20, 40], [20, 20, 20, 20]))
+console.log('Is the average of array 1 [20, 20, 20, 40] greater than array 2 [20, 20, 20, 20]?',isGreater([20, 20, 20, 40], [20, 20, 20, 20]))
 
 // 12. Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
 
@@ -152,10 +152,10 @@ function willBuyDrink (isHotOutside, moneyInPocket) {
     return isHotOutside && moneyInPocket > 10.50 ? true : false
  }
 
- console.log('Will buy drink?', willBuyDrink(true, 11))
- console.log('Will buy drink?', willBuyDrink(false, 11))
- console.log('Will buy drink?', willBuyDrink(true, 9))
- console.log('Will buy drink?', willBuyDrink(false, 9))
+ console.log('Will buy drink? (true, 11)', willBuyDrink(true, 11))
+ console.log('Will buy drink? (false, 11)', willBuyDrink(false, 11))
+ console.log('Will buy drink? (true, 9)', willBuyDrink(true, 9))
+ console.log('Will buy drink? (false, 9)', willBuyDrink(false, 9))
 
 // 13. Create a function of your own that solves a problem. In comments, write what the function does and why you created it.
 
@@ -165,7 +165,7 @@ function finishedWeek3 (functions, arrays) {
     return functions && arrays ? `You're done for the week!` : `You've got more to do!`
 }
 
-console.log(finishedWeek3(true, true))
-console.log(finishedWeek3(true, false))
-console.log(finishedWeek3(false, false))
+console.log('Did I finish week 3 openclass assignments? (true, true)',finishedWeek3(true, true))
+console.log('Did I finish week 3 openclass assignments? (true, false)',finishedWeek3(true, false))
+console.log('Did I finish week 3 openclass assignments? (false, false)',finishedWeek3(false, false))
 
