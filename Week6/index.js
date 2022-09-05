5b3f608b8d441f5579f4fa92a27e492dbcbb3b3d
sara.kuenzi <48669509+skuenzi@users.noreply.github.com>
2022-09-04 22:24:25 -0600

    added next turn button and functionality

--- Week6/index.js
+++ Week6/index.js
@@ -1,10 +1,11 @@
-const player1Card = document.getElementsByClassName("player-1-card")[0];
-const player2Card = document.getElementsByClassName("player-2-card")[0];
-const player1Score = document.getElementsByClassName("player-1-Score")[0];
-const player2Score = document.getElementsByClassName("player-2-Score")[0];
+const player1Card = document.querySelector(".player-1-card");
+const player2Card = document.querySelector(".player-2-card");
+const player1Score = document.querySelector(".player-1-score");
+const player2Score = document.querySelector(".player-2-score");
+const nextTurnBtn = document.querySelector(".next-turn-btn");
+const winner = document.querySelector(".winner");
 
 // create card class with suite/value
-
 class Card {
   constructor(suite, value) {
     this.suite = suite;
@@ -20,67 +21,71 @@ class Deck {
 
   // create a new deck
   createNewDeck() {
-    for(let i = 0; i < 13; i++) {
-        this.cards.push(new Card('hearts', i + 1))
-        this.cards.push(new Card('spades', i + 1))
-        this.cards.push(new Card('diamonds', i + 1))
-        this.cards.push(new Card('clubs', i + 1))
+    for (let i = 0; i < 13; i++) {
+      this.cards.push(new Card("Hearts", i + 1));
+      this.cards.push(new Card("Spades", i + 1));
+      this.cards.push(new Card("Diamonds", i + 1));
+      this.cards.push(new Card("Clubs", i + 1));
     }
-    return this.cards
-
+    return this.cards;
   }
 
   // shuffle deck
   shuffleDeck(cards) {
-    for (let i = cards.length-1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1))
-        let temp = cards[i]
-        cards[i] = cards[j]
-        cards[j] = temp
+    for (let i = cards.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = cards[i];
+      cards[i] = cards[j];
+      cards[j] = temp;
     }
 
-    return this.cards
+    return this.cards;
   }
-
 }
 
 //create player class with deck, points, winner
 class Player {
   constructor(cards) {
     this.cards = cards;
-    this.points = 0
+    this.points = 0;
   }
 }
 
+function play() {
+  let deck = new Deck();
+  let shuffledDeck = deck.shuffleDeck(deck.createNewDeck());
 
-function play(){
-    let deck = new Deck()
-    let shuffledDeck = deck.shuffleDeck(deck.createNewDeck())
-    
-    // create players
-    let player1 = new Player(shuffledDeck.slice(0,26))
-    let player2 = new Player(shuffledDeck.slice(26,52))
+  // create players
+  let player1 = new Player(shuffledDeck.slice(0, 26));
+  let player2 = new Player(shuffledDeck.slice(26, 52));
 
-    for (let i = 0; i < player1.cards.length; i++) {
-        console.log(player1.cards[i], player2.cards[i])
-        if (player1.cards[i].value > player2.cards[i].value) {
-            console.log('player1 wins')
-            player1.points++
-        } else if (player1.cards[i].value < player2.cards[i].value) {
-            console.log('player2 wins')
-            player2.points++
-        }
+    // 
+  let i = 0;
+  nextTurnBtn.addEventListener("click", () => {
+    if (i >= 26) {
+      if (player1.points > player2.points) {
+        winner.innerHTML = "Player 1 has won!";
+      } else if (player1.points < player2.points) {
+        winner.innerHTML = "Player 2 has won!";
+      } else {
+        winner.innerHTML = `It's a tie!`;
+      }
+      nextTurnBtn.disabled = true;
     }
 
-    if (player1.points > player2.points) {
-        console.log('Player 1 has won!') 
-    } else if (player1.points < player2.points) {
-        console.log('Player 2 has won!')
-    } else {
-        console.log(`It's a tie!`)
-    }
+    player1Card.innerHTML = `${player1.cards[i].value} of ${player1.cards[i].suite} `;
+    player2Card.innerHTML = `${player2.cards[i].value} of ${player2.cards[i].suite} `;
 
-    console.log(player1, player2)
+    if (player1.cards[i].value > player2.cards[i].value) {
+      player1.points++;
+    } else if (player1.cards[i].value < player2.cards[i].value) {
+      player2.points++;
+    }
+    // console.log(player1.cards[i], player2.cards[i])
+    i++;
+    player1Score.innerHTML = player1.points;
+    player2Score.innerHTML = player2.points;
+  });
 }
 
-play()
+play();
