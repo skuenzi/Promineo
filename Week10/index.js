18a20df236fa4cd803b01f8a27c87bdcfa18d998
sara.kuenzi <48669509+skuenzi@users.noreply.github.com>
2022-10-06 14:13:28 -0600

    finished form for week10

---
+++ Week10/index.js
@@ -0,0 +1,27 @@
+const catTable = document.querySelector("#cats-table");
+const addNewCat = document.querySelector("#add-cat-btn");
+
+const addCat = () => {
+  let newName = document.getElementById("name");
+  let coloring = document.getElementById("Coloring");
+  let breed = document.getElementById("Breed");
+  let temperment = document.getElementById("Temperment");
+
+
+  catTable.innerHTML += `
+    <tr>
+        <td>${newName.value}</td>
+        <td>${coloring.value}</td>
+        <td>${breed.value}</td>
+        <td>${temperment.value}</td>
+    </tr>
+    `;
+
+    newName.value = ''
+    coloring.value = ''
+    breed.value = ''
+    temperment.value = ''
+
+};
+
+addNewCat.addEventListener("click", addCat);
